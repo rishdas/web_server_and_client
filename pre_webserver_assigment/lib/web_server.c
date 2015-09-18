ad2254389fe0ee8cc1101c95d5079359d26b8d30
rishdas <sirshakdas@gmail.com>
2015-09-18 12:14:49 -0400

    Debug 3

--- pre_webserver_assigment/lib/web_server.c
+++ pre_webserver_assigment/lib/web_server.c
@@ -288,9 +288,11 @@ int handle_connection(int new_sock_conn, struct sockaddr cli_addr,
 						  req_info, debg_ofp);
 	fprintf(debg_ofp, "DEBUG: Persistant connection status : %d\n",
 		status);
+	fflush(debg_ofp);
 	close(new_sock_conn);
 	fprintf(debg_ofp, "DEBUG-2: Persistant connection status : %d\n",
 		status);
+	fflush(debg_ofp);
     }
     return status;
 }
