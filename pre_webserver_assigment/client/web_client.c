693ffa0831fc7ce0bbf5da174849f416ed1de558
rishdas <sirshakdas@gmail.com>
2015-09-18 02:22:32 -0400

    Web client enhnacements for project

--- pre_webserver_assigment/client/web_client.c
+++ pre_webserver_assigment/client/web_client.c
@@ -8,6 +8,7 @@
 #include<strings.h>
 
 FILE *debg_ofp;
+FILE     *ifp;
 int  client_sock_fd;
 typedef struct client_info_
 {
@@ -15,6 +16,7 @@ typedef struct client_info_
     char           *connection_type;
     char           *file_name;
     struct hostent *server_addr;
+    int            is_filename_in_disk;
 } client_info_t;
     
 int cleanup(int serv_sock_fd, FILE *debg_ofp)
@@ -30,7 +32,7 @@ int cleanup(int serv_sock_fd, FILE *debg_ofp)
 
 int parse_cmd_line_args(int argc, char *argv[],
 			client_info_t *client_info, FILE *debg_ofp)
-{
+{   
     if(argc< 5) {
 	fprintf(debg_ofp, "ERROR: Very few arguments "\
 		"the format for is \n"\
@@ -39,13 +41,21 @@ int parse_cmd_line_args(int argc, char *argv[],
 	return -1;
 	
     }
-    
+    bzero(client_info, sizeof(client_info));
     client_info->server_addr     = gethostbyname(argv[1]);
     client_info->port            = atoi(argv[2]);
     client_info->connection_type = argv[3];
     client_info->file_name       = argv[4];
-    
+    client_info->is_filename_in_disk = 0;
+    ifp = fopen(client_info->file_name, "r");
 
+    if (ifp == NULL) {
+	fprintf(debg_ofp,
+		"File not found in disk hence requesting from server\n");
+	client_info->is_filename_in_disk = 1;
+    }
+
+    
     fprintf(debg_ofp, "Parsed command line arguments\n");
     fprintf(debg_ofp, "Server Address: %s\t",
 	    client_info->server_addr->h_name);
@@ -93,9 +103,20 @@ int build_http_get_request(client_info_t req_info,
 			    char *resp_buf, FILE *debg_ofp)
 {
     int    http_version;
+    char   file_name[8192];
+    int    status;
+    if (req_info.is_filename_in_disk == 0) {
+	status = fscanf(ifp, "%s\n", file_name);
+	if (status <= 0) {
+	    return -1;
+	}
+    } else {
+	strncpy(file_name, req_info.file_name,
+		strlen(req_info.file_name));
+    }
     
     http_version = is_persistent(req_info);
-    sprintf(resp_buf, "GET\t/%s\t%s\r\n", req_info.file_name,
+    sprintf(resp_buf, "GET\t/%s\t%s\r\n", file_name,
 	    (http_version == 0)?"HTTP/1.1":"HTTP/1.0");
     sprintf(resp_buf, "%sHost: %s:%d\r\n", resp_buf,
 	    req_info.server_addr->h_name, req_info.port);
@@ -109,31 +130,11 @@ int build_http_get_request(client_info_t req_info,
     fprintf(debg_ofp, "INFO: GET Response: \n %s\n", resp_buf);
     return 0;
 }
-
-int main(int argc, char *argv[])
+int get_from_server_persistant(int client_sock_fd, client_info_t client_info,
+			       struct sockaddr_in server_addr, FILE *debg_ofp)
 {
-
-    int                status;
-    client_info_t      client_info;
-    char               file_name[] = "web_client_trace.log";
-    struct sockaddr_in server_addr;
     char               buf[8192];
-
-    debg_ofp = fopen(file_name, "w");
-    status = parse_cmd_line_args(argc, argv, &client_info, debg_ofp);
-    if(status != 0) {
-	fprintf(stderr, "ERROR: Invalid arguments check log\n");
-	cleanup(client_sock_fd, debg_ofp);
-	exit(1);
-    }
-    status = bootstrap_client(&client_sock_fd, client_info,
-			      &server_addr, debg_ofp);
-    if(status != 0) {
-	fprintf(stderr, "ERROR: Creating a socket\n");
-	fprintf(debg_ofp, "ERROR: Creating a socket\n");
-	exit(1);
-    }
-   
+    int                status;
     status = connect(client_sock_fd, (struct sockaddr*)&server_addr,
 		     sizeof(server_addr)); 
     if (status < 0) {
@@ -157,6 +158,70 @@ int main(int argc, char *argv[])
     }
     fprintf(stdout, "Incoming Message: \n %s", buf);
     fprintf(debg_ofp, "Incoming Message: \n %s", buf);
+    return 0;
+}
+
+int get_from_server_non_persistant(int client_sock_fd, client_info_t client_info,
+				   struct sockaddr_in server_addr, FILE *debg_ofp)
+{
+    char               buf[8192];
+    int                status;
+    status = connect(client_sock_fd, (struct sockaddr*)&server_addr,
+		     sizeof(server_addr)); 
+    if (status < 0) {
+	fprintf(stderr, "ERROR: Connecting to server\n");
+	fprintf(debg_ofp, "ERROR: Connecting to server\n");
+	exit(1);
+    }
+    status = build_http_get_request(client_info, buf, debg_ofp);
+    status = write(client_sock_fd, buf, strlen(buf));
+    if (status < 0) {
+	fprintf(stderr, "Error writing into the socket\n");
+	fprintf(debg_ofp, "Error writing into the socket\n");
+	exit(1);
+    }
+    bzero(buf, 8192);
+    status = read(client_sock_fd, buf, 8192);
+    if (status < 0) {
+	fprintf(stderr, "Error reading from the socket\n");
+	fprintf(debg_ofp, "Error reading into the socket\n");
+	exit(1);
+    }
+    fprintf(stdout, "Incoming Message: \n %s", buf);
+    fprintf(debg_ofp, "Incoming Message: \n %s", buf);
+    return 0;
+}
+int main(int argc, char *argv[])
+{
+
+    int                status;
+    client_info_t      client_info;
+    char               file_name[] = "web_client_trace.log";
+    struct sockaddr_in server_addr;
+    
+
+    debg_ofp = fopen(file_name, "w");
+    status = parse_cmd_line_args(argc, argv, &client_info, debg_ofp);
+    if(status != 0) {
+	fprintf(stderr, "ERROR: Invalid arguments check log\n");
+	cleanup(client_sock_fd, debg_ofp);
+	exit(1);
+    }
+    status = bootstrap_client(&client_sock_fd, client_info,
+			      &server_addr, debg_ofp);
+    if(status != 0) {
+	fprintf(stderr, "ERROR: Creating a socket\n");
+	fprintf(debg_ofp, "ERROR: Creating a socket\n");
+	exit(1);
+    }
+    if (is_persistent(client_info) == 0) {
+	get_from_server_persistant(client_sock_fd, client_info,
+				   server_addr, debg_ofp);
+    } else {
+	get_from_server_non_persistant(client_sock_fd, client_info,
+				       server_addr,debg_ofp);
+    }
+   
     cleanup(client_sock_fd, debg_ofp);
     return 0;
 }
